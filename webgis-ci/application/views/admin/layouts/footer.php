683fcc0f502cfe00e242e52f0ffbf8150ac671e9
misbahulms <37138953+misbahulms@users.noreply.github.com>
2021-07-11 20:27:53 +0700

    testing

---
+++ webgis-ci/application/views/admin/layouts/footer.php
@@ -0,0 +1,7 @@
+ <footer>
+  <div class="pull-right">
+    
+    <strong>Copyright &copy; 2020 | Nasrullah Siddik <a href="https://gitlab.com/as-shiddiq" target="_BLANK">Gitlab</a> | <a href="bit.ly/YTNSiddik" target="_BLANK">YTChannel</a>.</strong> | Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+  </div>
+  <div class="clearfix"></div>
+</footer>
